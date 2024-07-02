4a7c810602e2c2103183eda62da20470ec1e7d4c
sksamithi <vieira.thaism@gmail.com>
2024-07-02 15:46:42 -0300

    Desafio

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,9 +1,31 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
-        analisarCandidato(1900.0);
-        analisarCandidato(2000.0);
-        analisarCandidato(2500.0);
+        selecaoCandidatos();
+    }
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Maria","João","Thaís","Robson","Debora","Thiago","Thainara","Sarah"};
+        
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+    
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
     static void analisarCandidato (double salarioPretendido){
         double salarioBase = 2000.0;
