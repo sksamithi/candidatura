63c60611b1183721d29661bed3a0d187a8e83bb6
sksamithi <vieira.thaism@gmail.com>
2024-07-02 16:13:22 -0300

    Desafio

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,10 +1,52 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
         selecaoCandidatos();
+        imprimirSelecionados();
+
+        String [] candidatos = {"Maria","João","Thaís","Robson","Debora","Thiago","Thainara","Sarah"};
+        for (String candidato: candidatos){
+            entrandoContato(candidato);
+        }
     }
+
+    static void entrandoContato (String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            }else
+                System.out.println("Contato realizado com sucesso");
+        }while (continuarTentando && tentativasRealizadas <3);
+
+        if (atendeu){
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        }else {
+            System.out.println("Não conseguimos contato com " + candidato);
+        }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"Maria","João","Thaís","Robson","Debora","Thiago","Thainara","Sarah"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice");
+
+        for (int indice = 0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de número " + (indice+1) + " é " + candidatos[indice]);
+
+        }
+    }
+
     static void selecaoCandidatos(){
         String [] candidatos = {"Maria","João","Thaís","Robson","Debora","Thiago","Thainara","Sarah"};
         
